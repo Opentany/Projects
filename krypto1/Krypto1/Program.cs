c3e6b3176ca3140fc1c85ab5a14d5d2ecc96b5aa
Szary <Opentany.1972@gmail.com>
2016-10-30 11:51:50 +0100

    4miliardy kluczy w 80 godzin Great succes #fail

--- krypto1/Krypto1/Program.cs
+++ krypto1/Krypto1/Program.cs
@@ -18,93 +18,103 @@ namespace Krypto1
                              .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                              .ToArray();
         }
-        static Random random = new Random();
-        public static string GetRandomHexNumber(int digits)
-        {
-            byte[] buffer = new byte[digits / 2];
-            random.NextBytes(buffer);
-            string result = String.Concat(buffer.Select(x => x.ToString("X2")).ToArray());
-            if (digits % 2 == 0)
-                return result;
-            return result + random.Next(16).ToString("X");
-        }
+
         static void Main(string[] args)
         {
-            /*
-            int i = 0;
-            while (i>=0)
+            string Test = "Test";
+            using (
+                                System.IO.StreamWriter file =
+                                    new System.IO.StreamWriter(@"E:\Dokumenty\Uczelnia\Krypto\" + Test + ".txt",
+                                        true))
             {
-                string hex = i.ToString("X8");
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"E:\Dokumenty\Uczelnia\Krypto\Keys.txt", true))
-                {
-                    file.WriteLine(hex);
-                }
-                i++;
+                file.WriteLine(DateTime.Now);
             }
-            */
-            //*
-            string line;
-            System.IO.StreamReader file2 = new System.IO.StreamReader(@"E:\Dokumenty\Uczelnia\Krypto\Keys.txt");
-            while ((line = file2.ReadLine()) != null)
+            Bruteforce(0, 4294967296, Test);
+            Console.WriteLine("Koniec "+ DateTime.Now);
+        }
+
+        static void Bruteforce(Int64 start, Int64 stop, string filename)
+        {
+            Parallel.For(start,stop,delegate(Int64 j)
+            //for (Int64 j = start; j < stop; j++)
+
             {
+                if (j% 1000000/*1073741824*/ == 0)
+                {
+                    Console.WriteLine("Klucz " + j + " " + DateTime.Now);
+                    using (
+                        System.IO.StreamWriter file =
+                            new System.IO.StreamWriter(@"E:\Dokumenty\Uczelnia\Krypto\" + filename + ".txt",
+                                true))
+                    {
+                        file.WriteLine("Klucz " + j + " " + DateTime.Now);
+                    }
+                }
                 try
                 {
-
                     //string original = "Ala ma kota. Kot ma, ale... to jednak ona go posiada. Jednakże gdy przeczytamy to ponownie to...";
 
                     // Create a new instance of the Aes
                     // class.  This generates a new key and initialization 
                     // vector (IV).
+                    string hex = j.ToString("X8");
                     string SufKey = "1fb7d76820734c94b97233a15022dd4cc2f827e7e2195685fdf848b4";
                     string Iv = "9022544f4cb454ec88880674f06dab4d";
-                    string PreKey = line;
+                    string PreKey = hex;
                     string Key = PreKey + SufKey;
 
                     byte[] byteKey = StringToByteArray(Key);
                     byte[] byteIV = StringToByteArray(Iv);
                     using (Aes myAes = Aes.Create())
                     {
-                        myAes.KeySize = 256;
-                        foreach (var VARIABLE in myAes.LegalKeySizes)
-                        {
-                            Console.WriteLine(VARIABLE.MaxSize);
-                        }
                         // Encrypt the string to an array of bytes.
                         //byte[] encrypted = EncryptStringToBytes_Aes(original, byteKey, byteIV);
-                        string input = "00011101001010110010010111111100101101011100110111101110001101111000100000101010111100011100100100101011100111011110001010101011111110100110111101110101110010011001001000101101110010001101011111101001010101000011111100000110001001110100100100100111110111001101101011001110011001100000101111110111010011000100101100011111101100101111011100101001010010100011101000000000001100100111001001000100010111111110100111000111110001000011001110110000011100100100110101101001011001010101110110100100101001100101011010010000";
-                        int numOfBytes = input.Length / 8;
+                        string input =
+                            "00011101001010110010010111111100101101011100110111101110001101111000100000101010111100011100100100101011100111011110001010101011111110100110111101110101110010011001001000101101110010001101011111101001010101000011111100000110001001110100100100100111110111001101101011001110011001100000101111110111010011000100101100011111101100101111011100101001010010100011101000000000001100100111001001000100010111111110100111000111110001000011001110110000011100100100110101101001011001010101110110100100101001100101011010010000";
+                        int numOfBytes = input.Length/8;
 
                         byte[] bytes = new byte[numOfBytes];
                         for (int i = 0; i < numOfBytes; ++i)
                         {
-                            bytes[i] = Convert.ToByte(input.Substring(8 * i, 8), 2);
+                            bytes[i] = Convert.ToByte(input.Substring(8*i, 8), 2);
                         }
                         byte[] encrypted = bytes;
                         // Decrypt the bytes to a string.
-                        string roundtrip = DecryptStringFromBytes_Aes(encrypted, byteKey, byteIV);
+
+                        var roundtrip = DecryptStringFromBytes_Aes(encrypted, byteKey, byteIV);
+
+
 
                         //Display the original data and the decrypted data.
                         //Console.WriteLine("Original:   {0}", original);
                         //Console.WriteLine("Round Trip: {0}", roundtrip);
-                        using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"E:\Dokumenty\Uczelnia\Krypto\Dekrypto.txt", true))
+                        string sOut = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(roundtrip));
+                        if (!sOut.Contains("??"))
                         {
-                            file.WriteLine(roundtrip);
+                            using (
+                                System.IO.StreamWriter file =
+                                    new System.IO.StreamWriter(@"E:\Dokumenty\Uczelnia\Krypto\" + filename + ".txt",
+                                        true))
+                            {
+                                file.WriteLine(sOut);
+                            }
                         }
                     }
 
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("Error: {0}", e.Message);
+
+                    //
                 }
-                //Console.ReadKey();
-                
+            //}
 
+
+
+            });
             }
-            //*/
 
-        }
-        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+            static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
         {
             // Check arguments.
             if (plainText == null || plainText.Length <= 0)
